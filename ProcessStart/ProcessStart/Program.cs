49ac28a4819a1b7a6784c203e7bcac765434af14
Eduardo Spaki <spoky.br@gmail.com>
2017-08-21 20:50:22 +0100

    first commit

---
+++ ProcessStart/ProcessStart/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace ProcessStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("http://www.almocando.com.br/");
+            Process.Start("explorer.exe");
+            Process.Start(@"C:\Windows\Web\Wallpaper\Theme1\img1.jpg");
+        }
+    }
+}
